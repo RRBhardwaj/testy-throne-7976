28f06091bee0d54a972d0367436895f3a9ba5125
Rishi <rishirajbhardwaj20@gmail.com>
2022-11-10 18:55:26 +0530

    day-2

---
+++ my-licious-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar title="Licious" aboutText="Login" aboutText1="Categories"/>
+    </div>
+  );
+}
+
+export default App;
